df3833df20142216520bca947b4f9f1c87caa040
Pad <145267541+PadjokeJ@users.noreply.github.com>
2025-04-03 11:10:14 +0200

    try fixing blank page on pc

--- styler.js
+++ styler.js
@@ -4,4 +4,4 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
     sheet = "mobile.css";
     document.getElementById("pagestyle").setAttribute("href", sheet);
 }
-document.getElementById("content").style.opacity = 1;
+document.body.getElementById("content").style.opacity = 1;
