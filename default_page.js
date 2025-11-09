eda33b95a5b9c91a64fa9a33c5cef3c87a24f2b4
PadjokeJ <jonatan@padjokej.dev>
2025-11-09 16:38:19 +0100

    fix wrong definition for for loop var

--- default_page.js
+++ default_page.js
@@ -24,7 +24,7 @@ const navbarToggle = document.getElementById("burgermenu");
 navbarToggle.addEventListener("click", () => {
   let all = document.getElementsByClassName("navli");
 
-  for (int navli = 0; navli < all.length; navli++) {
+  for (let navli = 0; navli < all.length; navli++) {
     all.item(navli).classList.toggle("navli-show");
   }
 });
