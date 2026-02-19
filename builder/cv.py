21247e7acbb643c75b9e5c8e4720b300fe625442
PadjokeJ <jonatan@padjokej.dev>
2026-02-19 22:05:36 +0100

    fix cursor alignement

--- builder/cv.py
+++ builder/cv.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
   for x in formatted:
     html += x + '\n'
   html += "</span>"
-  html += "git@padjokej.dev: Life$ <span id=\"user-input\"></span><span id=\"cursor\" style=\"animation: blink 1s infinite\">|</span>"
+  html += "git@padjokej.dev: Life$ <span id=\"user-input\"></span><span id=\"cursor\" style=\"animation: blink 1s infinite\"></span>"
   html += "</code></pre>"
   
   
