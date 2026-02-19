1f3b622a994e07c9e91be8852db83dc24c9d36f7
PadjokeJ <jonatan@padjokej.dev>
2026-02-19 21:32:51 +0100

    remove styler.js

--- styler.js
+++
@@ -1,6 +0,0 @@
-let sheet;
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) 
-{
-    sheet = "mobile.css";
-    document.getElementById("pagestyle").setAttribute("href", sheet);
-}
