2d14e41218b968511a63a5c3360be4b3fc363f6c
PadjokeJ <jonatan@padjokej.dev>
2026-04-03 19:57:37 +0200

    feat: color changes depending on tech used

--- projects.js
+++ projects.js
@@ -1,69 +1,86 @@
+const colors = {
+  java: "#E11F21",
+  c-sharp: "#9D76D6",
+  python: "#336F9F",
+  javascript: "#EFD917",
+  rust: "#EF4900"
+}
+
+function get_color(key) {
+  let k = key.toLowerCase();
+  if (k == "c#") k = "c-sharp";
+
+  return colors[key];
+}
+  
+
 function proj_button(project) {
-    const div_spawn = document.getElementById("buttons-container");
+  const div_spawn = document.getElementById("buttons-container");
 
-    lnk = document.createElement("a");
-    lnk.target = "_blank";
-    lnk.href = project.URL;
-    lnk.classList.add("project");
-    lnk.id = project.Id;
+  lnk = document.createElement("a");
+  lnk.target = "_blank";
+  lnk.href = project.URL;
+  lnk.classList.add("project");
+  lnk.id = project.Id;
 
-    div_spawn.append(lnk);
+  div_spawn.append(lnk);
 
-    tec = document.createElement("div");
-    tec.classList.add("tech");
-    tec.innerHTML = project.Tech;
+  tec = document.createElement("div");
+  tec.classList.add("tech");
+  tec.innerHTML = project.Tech;
+  tec.style.backgroundColor = get_color(project.Tech);
 
-    lnk.append(tec);
+  lnk.append(tec);
 
-    img = document.createElement("img");
-    img.src = "images/" + project.Image;
-    img.style.width = "312px";
-    img.style.height = "256px";
-    img.style.transition = "transform ease 0.2s";
-    img.id = "image-" + project.Id;
-    img.classList.add("project-image");
+  img = document.createElement("img");
+  img.src = "images/" + project.Image;
+  img.style.width = "312px";
+  img.style.height = "256px";
+  img.style.transition = "transform ease 0.2s";
+  img.id = "image-" + project.Id;
+  img.classList.add("project-image");
 
-    lnk.append(img);
+  lnk.append(img);
 
-    slider = document.createElement("div");
-    slider.style.transform = "translateY(0px)";
-    slider.id = `slider-${project.Id}`;
-    slider.classList.add("sliding");
-    lnk.append(slider);
+  slider = document.createElement("div");
+  slider.style.transform = "translateY(0px)";
+  slider.id = `slider-${project.Id}`;
+  slider.classList.add("sliding");
+  lnk.append(slider);
 
-    title = document.createElement("p");
-    title.classList.add("project-title");
-    title.innerHTML = project.Title;
-    slider.append(title);
+  title = document.createElement("p");
+  title.classList.add("project-title");
+  title.innerHTML = project.Title;
+  slider.append(title);
 
-    desc = document.createElement("p");
-    desc.classList.add("project-description");
-    desc.innerHTML = project.Description;
-    slider.append(desc);
+  desc = document.createElement("p");
+  desc.classList.add("project-description");
+  desc.innerHTML = project.Description;
+  slider.append(desc);
 
-    lnk.addEventListener("mouseenter", (event) => {
-        id = event.target.id;
-        sl = document.getElementById("slider-" + id);
-        sl.style.transform = "translateY(-50%)";
-        im = document.getElementById("image-" + id);
-        im.style.transform = "scale(1.05)";
-    });
-    lnk.addEventListener("mouseleave", (event) => {
-        id = event.target.id;
-        sl = document.getElementById("slider-" + id);
-        sl.style.transform = "translateY(0)";
-        im = document.getElementById("image-" + id);
-        im.style.transform = "scale(1)";
-    });
+  lnk.addEventListener("mouseenter", (event) => {
+    id = event.target.id;
+    sl = document.getElementById("slider-" + id);
+    sl.style.transform = "translateY(-50%)";
+    im = document.getElementById("image-" + id);
+    im.style.transform = "scale(1.05)";
+});
+lnk.addEventListener("mouseleave", (event) => {
+    id = event.target.id;
+    sl = document.getElementById("slider-" + id);
+    sl.style.transform = "translateY(0)";
+    im = document.getElementById("image-" + id);
+    im.style.transform = "scale(1)";
+  });
 }
 
 function spawn_buttons(json) {
-    projects = json.Projects;
-    for (i in projects){
-        proj_button(projects[i]);
-    }
+  projects = json.Projects;
+  for (i in projects){
+    proj_button(projects[i]);
+  }
 }
 
 fetch("./projects.json")
-    .then((response) => response.json())
-    .then((json) => spawn_buttons(json));
+  .then((response) => response.json())
+  .then((json) => spawn_buttons(json));
